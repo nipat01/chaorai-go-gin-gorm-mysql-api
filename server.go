c70eecbcca40b898226b178074b0c95af83e9bcc
nipat01 <nipat.pueksasuwan@gmail.com>
2022-07-23 23:00:39 +0700

    add env

--- server.go
+++ server.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/nipat01/chaorai-go-gin-gorm-mysql-api/database"
 )
 
 func main() {
 	server := gin.Default()
+
+	var envs map[string]string
+	envs, err := godotenv.Read(".env")
+	if err != nil {
+		log.Println("Error to load .env file")
+	}
+	log.Println(envs)
 	
-	database.Connect("root:123456@tcp(localhost:3306)/db-mysql-dev?parseTime=true")
+	dbConnect := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", envs["DB_USER"], envs["DB_PASS"], envs["DB_URL"], envs["DB_PORT"], envs["DB_NAME"])
+	log.Println("dbConnect: ", dbConnect)
+	database.Connect(dbConnect)
 	database.Migrate()
 
 	server.GET("/", func(ctx *gin.Context) {
